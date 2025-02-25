17e33ef4f17079d6f1ca5689728bc87a8d34e392
dgoodwin-maker <dgoodwin3@una.edu>
2025-02-25 02:51:28 -0600

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log("Document ready!");
+
+    // Easter egg
+    console.log("You found the easter egg. Good job! Follow the following log please!");
+    console.log('Click link in console: https://youtu.be/xvFZjo5PgG0?si=JoO3oD9wfcI5qrFC');
+  });
