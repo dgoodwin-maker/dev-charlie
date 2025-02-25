bb5afcb24cd509ffabae18d530213aee5da6e9e5
dgoodwin-maker <dgoodwin3@una.edu>
2025-02-25 04:15:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ $(document).ready(function() {
     // Easter egg
     console.log("You found the easter egg. Good job! Follow the following log please!");
     console.log('Click link in console: https://youtu.be/xvFZjo5PgG0?si=JoO3oD9wfcI5qrFC');
-  });
+  }); 
